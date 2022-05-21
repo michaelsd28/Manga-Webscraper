5f844781b39b0b7f92259cd41ef68b939e45c204
michaelsd28 <rd28@hotmail.es>
2022-05-21 04:27:36 -0400

    first

--- lib/widgets/MangaGallery/ImageListView.dart
+++ lib/widgets/MangaGallery/ImageListView.dart
@@ -5,15 +5,13 @@ import 'package:mymangawebsite/services/ImageService.dart';
 
 import '../../services/DatabaseService.dart';
 
-
-
-
-
 class ImageListView extends StatefulWidget {
   final String ChapterLink;
   final bool isBase64;
 
-  const ImageListView({Key? key, required this.ChapterLink, required this.isBase64}) : super(key: key);
+  const ImageListView(
+      {Key? key, required this.ChapterLink, required this.isBase64})
+      : super(key: key);
 
   @override
   State<ImageListView> createState() => _ImageListViewState();
@@ -33,17 +31,13 @@ class _ImageListViewState extends State<ImageListView> {
 
     ///var images64 = await ImageService().ListToBase64(widget.ChapterLink);
 
-
-
     images.forEach((element) {
       print(" imageList Element: *$element* ");
     });
 
-
-
     setState(() {
       imageList = images;
-     // imageList = images64;
+      // imageList = images64;
     });
   }
 
@@ -61,16 +55,20 @@ class _ImageListViewState extends State<ImageListView> {
     }*/
 
     return Column(
-
-      children: imageList.map((element) => Image.network(element!,
-        filterQuality: FilterQuality.high,
-        errorBuilder: (context, error, stackTrace) {
-          print("Image Error: $error");
-          return Image.asset("assets/topbar/image64.png",height: 30, width: 30,);
-        },
-
-        )).toList(),
-
+      children: imageList
+          .map((element) => Image.network(
+                element!,
+                filterQuality: FilterQuality.high,
+                errorBuilder: (context, error, stackTrace) {
+                  print("Image Error: $error");
+                  return Image.asset(
+                    "assets/topbar/image64.png",
+                    height: 30,
+                    width: 30,
+                  );
+                },
+              ))
+          .toList(),
     );
   }
 }
